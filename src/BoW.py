4563bd3c549305bf4a33b6268209841bc1aa8180
AhaanB29 <ahaanbanerjee11@gmail.com>
2025-08-01 03:00:32 +0530

    Data Association - RANSAC optimisation

---
+++ src/BoW.py
@@ -0,0 +1,146 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+from sklearn.cluster import KMeans
+from collections import defaultdict
+import pickle
+######################################## Creation BoW #############################
+class BoW:
+    def __init__(self,branching_factor=10,max_depth = 3):
+        self.max_depth = max_depth
+        self.bf = branching_factor
+        self.tree = None
+        self.leaf_count = 0
+    
+    class Node:
+        def __init__(self,centroid=None,depth=0, children=None):
+            self.depth = depth
+            self.centroid = centroid
+            self.children = children if children is not None else []
+            self.is_leaf = False
+            self.img_id_lst=[]
+    def fit(self,descriptors):
+        self.leaf_count=0
+        self.tree = self.recursive_clustering(descriptors,0)
+
+    def recursive_clustering(self,descriptors,depth):
+        if((depth == self.max_depth) or (len(descriptors)<self.bf)):
+           node = self.Node(centroid = np.mean(descriptors,axis=0),depth=depth,children=None)
+           node.is_leaf = True
+           self.leaf_count+=1
+           return node
+        
+        k = min(self.bf,len(descriptors))
+        clustering = KMeans(n_clusters=k, random_state=42, n_init=10)
+        labels = clustering.fit_predict(descriptors)
+
+        node = self.Node(children = [], depth = depth)
+        node.centroid = clustering.cluster_centers_
+        for c_id in range(k):
+            cluster_desc = descriptors[labels==c_id] # this is to get the subset of the descp in that particular cluster ID
+            if(len(cluster_desc) >0):
+                child = self.recursive_clustering(cluster_desc,depth+1)
+                node.children.append(child)
+        return node
+
+    def find_leaf(self,descriptor):
+        node= self.tree
+        while not node.is_leaf and node.children:
+            # calculate the diff with all the centroids
+            centroids = node.centroid
+            values = np.linalg.norm(centroids - descriptor, axis=1)
+            idx = np.argmin(values)
+            node = node.children[idx]
+        return node
+    
+    def inv_index(self, image_name, descriptors):
+        """Assign image_name to all leaves matching its descriptors."""
+        for d in descriptors:
+            leaf = self.find_leaf(d)
+            if image_name not in leaf.img_id_lst:
+                leaf.img_id_lst.append(image_name)
+
+    def query(self, query_descriptors):
+        """Return filenames ranked by vote count for query descriptors."""
+        votes = {}
+        for d in query_descriptors:
+            leaf = self.find_leaf(d)
+            for img_name in leaf.img_id_lst:
+                if img_name not in votes.keys():
+                    votes[img_name] = 1
+                else :
+                    votes[img_name]+=1
+        # sort by descending votes
+        return sorted(votes.items(), key=lambda x: x[1], reverse=True)
+        #return votes
+##############################################################################
+    
+def feature_extractor(dir_path,orb):
+    all_descriptors = []
+    # Use Feature Extractor to Key key points and descriptors
+    img_id=[]
+    for img_name in os.listdir(dir_path):
+        img = cv2.imread(dir_path+'/'+img_name,cv2.IMREAD_GRAYSCALE)
+        kp,descriptors = orb.detectAndCompute(img,None)
+        all_descriptors.append(descriptors)
+        img_id.append(img_name)
+    return img_id,np.array(all_descriptors)
+
+
+def scene_graph(img_id,descriptors,tree):
+    graph = defaultdict(list)
+    for id,desc in zip(img_id,descriptors):
+        votes = tree.query(desc)
+        for other,freq in votes:
+            if id!=other:
+                score = freq/len(desc)
+                if(score >= 0.095):
+                    graph[id].append(other)
+    return graph
+
+def BF_matches(query_img,candidate_img,path,orb):
+    q_img = cv2.imread(path+query_img,cv2.IMREAD_GRAYSCALE)
+    c_img = cv2.imread(path+candidate_img,cv2.IMREAD_GRAYSCALE)
+    kp_q,descriptors_q = orb.detectAndCompute(q_img,None)
+    kp_c,descriptors_c = orb.detectAndCompute(c_img,None)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    matches = bf.match(descriptors_c, descriptors_q)
+    # Apply ratio test
+    good = sorted(matches,key=lambda x:x.distance)
+    return good
+
+
+def RANSAC_refinement_Graph(graph,path,orb):
+    verified_graph = {}
+    for query_img, candidates in graph.items():
+        for candidate_img in candidates:
+            matches = BF_matches(query_img, candidate_img,path,orb)
+            inliers, _ = ransac_geometric_verification(matches)
+            if len(inliers) >= min_inliers:  # e.g., 15-30
+                verified_graph[query_img].append(candidate_img)
+    
+    return verified_graph
+###############################################################
+if __name__ == '__main__':
+    path = "/media/ahaanbanerjee/Crucial X9/SfM/Data/train/church/images/"
+    orb = cv2.ORB_create(nfeatures=2000)
+    img_id , descprs = feature_extractor(path,orb)
+    flattned_desc = descprs.reshape(-1,32)
+    bow = BoW(branching_factor=15,max_depth=8)
+    # bow.fit(flattned_desc)
+    # print(bow.leaf_count)
+    # #print(bow.tree.centroid)
+    # for img_name, descs in zip(img_id, descprs):
+    #     bow.inv_index(img_name, descs)
+
+    # with open('hkm_tree.pkl', 'wb') as f:
+    #     pickle.dump(bow, f)
+
+# --- Later, to load it back ---
+    with open('hkm_tree.pkl', 'rb') as f:
+        loaded_tree = pickle.load(f)
+    
+    sc_grph = scene_graph(img_id,descprs,loaded_tree)
+    RANSAC_refinement_Graph(sc_grph,path,orb)
